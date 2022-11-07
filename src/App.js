be6c5cdbf82ff3dd4b4683e6df4d7083493adebe
Jtawfik04 <94028535+Jtawfik04@users.noreply.github.com>
2022-11-06 22:58:04 -0500

    Tailwind

--- src/App.js
+++ src/App.js
@@ -1,33 +1,21 @@
 import React from "react";
 
-function App() {
+ export function App() {
   return (
     <>
-
-      <div class="bg-white dark:bg-gray-800 overflow-hidden relative">
+      <div class="bg-white dark:bg-gray-800 max-h-full">
         <div class="text-start w-1/2 py-12 px-4 sm:px-6 lg:py-16 lg:px-8 z-20">
-          <h2 class="text-3xl font-extrabold text-black dark:text-white sm:text-4xl">
+          <h2 class="text-3xl font-extrabold text-black dark:text-white sm:text-4xl text-center">
             <span class="block">
               Want to get your car detailed ?
             </span>
+            <br />
             <span class="block text-indigo-500">
-              It's today or never.
+              Contact Fresh Off The Lot Detailing!
             </span>
           </h2>
-          <p class="text-xl mt-4 text-gray-400">
-            I had noticed that both in the very poor and very rich extremes of society the mad were often allowed to mingle freely
-          </p>
-          <div class="lg:mt-0 lg:flex-shrink-0">
-            <div class="mt-12 inline-flex rounded-md shadow">
-              <button type="button" class="py-4 px-6  bg-indigo-600 hover:bg-indigo-700 focus:ring-indigo-500 focus:ring-offset-indigo-200 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2  rounded-lg ">
-                Get started
-              </button>
-            </div>
-          </div>
         </div>
-        <img src="/images/object/1.png" class="absolute h-full max-w-1/2 hidden lg:block right-0 top-0" />
       </div>
-
     </>
   );
 }
